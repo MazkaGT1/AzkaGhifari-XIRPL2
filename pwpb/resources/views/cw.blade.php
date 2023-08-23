5d710e5074be9677511056a5ab509ae26fb15db3
MazkaGT <azkaganteng1406@gmail.com>
2023-08-23 12:15:57 +0700

    initial commit

---
+++ pwpb/resources/views/cw.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width", initial-scale=1,0">
+        <link rel="stylesheet" href="style/stylee1.css">
+</head>
+<body>
+    <header>
+        <h1>CV</h1>
+    </header>
+<hr></hr>
+<br>
+<img src="okah.jpg" alt="" width="100" class="image">
+    <div class="yey">
+    <table border="0" width="500" > 
+        <div class=""
+       <h2><tr><td>Nama</td> <td> :Ahmad dzidan logika sanma</td></tr></h2>
+        <tr><td>Kelas</td> <td>: RPL 2</td></tr>
+        <tr><td>Jurusan</td> <td>: Rekayasa Perangkat Lunak</td></tr>
+        <tr><td>Alamat</td> <td>: JL Asepberlian</td></tr>
+        <tr><td>Agama</td> <td>: Islam</td></tr>
+        <tr><td>Tempat Tanggal lahir</td> <td>: Bandung 26 juli 2006 </td></tr>
+        <tr><td>No telpon</td> <td>: 083876971302</td></tr>
+        <tr><td>NISN</td> <td>: 112233445566</td></tr>
+        <tr><td>email</td> <td>: ahmaddzidanlogikasanma@gmail.com</td></tr>
+    </table>
+    </div>
+    <hr></hr>
+</body>
+</html>
