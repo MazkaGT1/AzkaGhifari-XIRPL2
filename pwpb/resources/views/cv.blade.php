b6e5df24c1877f78adaef02a532d79dac282bc15
MazkaGT <azkaganteng1406@gmail.com>
2023-09-13 14:01:43 +0700

    feat: add new file cv

---
+++ pwpb/resources/views/cv.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="smk2.png">
+    <link rel="stylesheet" href="/css/stylee.css">
+    <title>CV</title>
+</head>
+<body>
+    <header>
+        <nav class="body1">
+        <div class="logo">
+            <div class="image1">
+                    <img src="smk2.png" alt="">
+            </div>
+                <div class="judul">
+                    <h1>Curriculum Vitae</h1>
+                </div>
+        </div>
+    <div class="links">
+                <a href="#tentang">Tentang</a>
+                <a href="profile_pplg#sosmed">Kontak</a>
+                <a href="profile_pplg">Profile PPLG</a>
+            </div>
+        </nav>
+    </header>
+    <h2>CV</h2>
+    <div class="box">
+    <div class="isi">
+    <h3>Nama                    =  Mohammad Azka Ghifari</h3>
+    <h3>Tempat/Tanggal Lahir    =  Bandung/20 Mei 2006</h3>
+    <h3>Jenis Kelamin           =  Laki Laki</h3>
+    <h3>Sekolah                 =  SMKN 2 Bandung</h3>
+    <h3>Jurusan                 =  Rekayasa Perangkat Lunak</h3>
+    <h3>Negara                  =  Indonesia</h3>
+    <h3>Agama                   =  Islam</h3>
+    <h3>Status                  =  Masih Sekolah</h3>
+    <h3>NISN                    =  123456789</h3>
+    <h3>Alamat                  =  Jalan Kemenangan</h3>
+    <h3>Nomer Telfon            =  08888888888</h3>
+    <h3>Email                   =  akugantengkatawindah@gmail.com</h3>
+    </div>
+    <div class="gambar">
+    <img src="mazkagt.jpg" alt="" class="image">
+    </div>
+    </div>
+    <section>
+    <div class="Tentang">
+        <div class="link">
+            <a href="#tentang"></a>
+        </div>
+    <div class="overlay" id="tentang">
+        <div class="popup">
+            <a class="close" href="#home">&times;</a>
+            <div class="teksnya">
+            <h2>By Mohammad Azka Ghifari</h2>
+            <h2>XI RPL 2</h2>
+            <h2>Absen 18</h2>
+            </div>
+        </div>
+    </div>
+</section>
+            <div class="circle1"></div>
+                <div class="circle2"></div>
+</body>
+</html>
