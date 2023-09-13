8e8d2d66a66dae1bbc98be09ba57eba005b5a818
MazkaGT <azkaganteng1406@gmail.com>
2023-09-13 13:57:28 +0700

    feat: add new program in pwpb

---
+++ pwpb/resources/views/profile_pplg.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile PPLG</title>
+    <link rel="icon" href="smk2.png">
+    <link rel="stylesheet" href="/css/cssprofile_pplg.css">
+</head>
+<body>                                
+        <header>
+        <nav class="body1">
+            <div class="image1">
+                <img src="smk2.png" alt="">
+            <div class="logo-text">
+                <h1 class="logo-title">PPLG</h1>
+                <p class="logo-subtitle">(Pengembangan Perangkat Lunak dan Game)</p>
+            </div>
+            </div> 
+            <div class="links">
+                <a href="#home">Beranda</a>
+                <a href="#tentang">Tentang</a>
+                <a href="#sosmed">Kontak</a>
+                <a href="cv">CV</a>
+            </div>
+        </nav>
+    </header>
+    <div id="home" class="isinya">
+    <h1> Profile PPLG</h1>
+    <p class="isinyaa">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis molestiae doloremque, maxime officiis a voluptatibus quidem distinctio. Excepturi tempora nisi quibusdam porro saepe et, inventore rem itaque harum dicta quaerat.</p> <br> <br> <p class="sipalingcpp">Cout << "Apa Aja Boleh...";
+    </p>
+    </div>
+    <section>
+    <div class="Tentang">
+        <div class="link">
+            <a href="#tentang"></a>
+        </div>
+    <div class="overlay" id="tentang">
+        <div class="popup">
+            <a class="close" href="#home">&times;</a>
+            <div class="teksnya">
+            <h2>By Mohammad Azka Ghifari</h2>
+            <h2>XI RPL 2</h2>
+            <h2>Absen 18</h2>
+            </div>
+        </div>
+    </div>
+            </section>
+            <h1 class="title-p">Foto Orang Ganteng Di RPL 2</h1>
+    <section id="gambarnya" class="gambarnya">
+        <div class="card">
+            <div class="gambar1">
+                <div class="link">
+                            <img src="gambarmaul.jpg" alt="">
+                            <div class="judul-gambar1">Gambar Maul</div>
+                </div>
+                </div>
+                <div class="gambar2">
+                    <div class="link">
+                                <img src="gambarmunthe.jpg" alt="">
+                                <div class="judul-gambar1">Gambar Munthe</div>
+                    </div>
+                    </div>
+                    <div class="gambar3">
+                        <div class="link">
+                                    <img src="gambaroka.jpg" alt="">
+                                    <div class="judul-gambar1">Gambar Oka</div>
+                        </div>
+                        </div>
+                    </div>
+                </section>
+                    <footer>
+                        <div class="sosmed" id="sosmed">
+                            <h1>SOSIAL MEDIA</h1>
+                            <a href="https://instagram.com/azka.ghifari_" target="_blank" rel="noopener noreferrer"><img src="ig.png" alt=""></a>
+                        </div>
+                    </footer>
+                    <div id="copyright">
+                        &copy;2023 Mazka GT Well
+                    </div>
+                <div class="circle1"></div>
+                <div class="circle2"></div>
+</body>
+</html>
